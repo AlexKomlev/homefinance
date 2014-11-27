e4746bf54a3b9e7af81b8ffa4dad1fab02d475b3
Aleksey Komlev <AlexKomlev@gmail.com>
2014-11-28 01:17:32 +0300

    rf

---
+++ src/main/java/org/komlev/hf/dao/AccountDao.java
@@ -0,0 +1,38 @@
+package org.komlev.hf.dao;
+
+import org.komlev.hf.domain.Account;
+import org.komlev.hf.domain.AccountTypeE;
+
+import java.util.List;
+
+/**
+ * Interface for Accounting service.
+ *
+ * @author <a href="mailto:AlexKomlev@rambler.ru">Aleksey Komlev</a>
+ * @version 24.05.2014
+ */
+public interface AccountDao {
+
+    /**
+     * Returns all accounts.
+     *
+     * @return List of accounts.
+     */
+    List<Account> getAccounts();
+
+    /**
+     * Returns account by specified Id.
+     *
+     * @param id Account's Id.
+     * @return Account object.
+     */
+    Account getAccount(Long id);
+
+    /**
+     * Returns account by specified name.
+     *
+     * @param name Account's name.
+     * @return Account object.
+     */
+    Account getAccount(String name);
+}
---
+++ src/main/java/org/komlev/hf/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,77 @@
+package org.komlev.hf.dao.impl;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.komlev.hf.dao.AccountDao;
+import org.komlev.hf.domain.Account;
+import org.komlev.hf.domain.AccountTypeE;
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+
+import java.util.List;
+
+/**
+ * Implementation of {@link org.komlev.hf.dao.AccountDao}.
+ *
+ * @author <a href="mailto:AlexKomlev@rambler.ru">Aleksey Komlev</a>
+ * @version 24.05.2014
+ */
+public class AccountDaoImpl extends HibernateDaoSupport implements AccountDao {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    /**
+     * {@inheritDoc}
+     */
+    public List<Account> getAccounts() {
+        Session session = getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        List<Account> accounts = null;
+        try {
+            transaction.begin();
+            accounts = session.createCriteria(Account.class).list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+        }
+        return accounts;
+    }
+
+    @Override
+    /**
+     * {@inheritDoc}
+     */
+    public Account getAccount(Long id) {
+        Account result = null;
+        Session session = getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        try {
+            transaction.begin();
+            result = (Account) session.get(Account.class, id);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+        return result;
+    }
+
+    @Override
+    /**
+     * {@inheritDoc}
+     */
+    public Account getAccount(String name) {
+        Account result = null;
+        Session session = getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        try {
+            transaction.begin();
+            result = (Account) session.createCriteria(Account.class).add(Restrictions.eq("name", name)).uniqueResult();
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+        return result;
+    }
+}
